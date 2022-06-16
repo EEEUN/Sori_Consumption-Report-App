be2c351363274610df43b32ec5344867821acc73
EEEUN <eeeun0929@gmail.com>
2022-06-16 09:00:42 +0900

    Add bottomNavigationBar Layout - 하단 네비게이션바 생성 - '내역', '분석', '설정' 화면 분리 구현

--- app/src/main/java/com/example/sori/MainActivity.java
+++ app/src/main/java/com/example/sori/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.sori;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout homeLayout;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeLayout = findViewById(R.id.home_ly);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationViewListener();
+        bottomNavigationView.setSelectedItemId(R.id.tab_list);  //맨 처음 시작할 탭 설정
+    }
+
+    // 하단 네비게이션 바의 아이콘을 선택하면 해당 페이지로 이동
+    private void bottomNavigationViewListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab_list:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.home_ly, new ListFragment())
+                                .commit();
+                        break;
+                    case R.id.tab_chart:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.home_ly, new ChartFragment())
+                                .commit();
+                        break;
+                    case R.id.tab_setting:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.home_ly, new SettingFragment())
+                                .commit();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
