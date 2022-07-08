bb4934204d3d60df3395dd9c0fee3403bb53b049
EEEUN <eeeun0929@gmail.com>
2022-07-08 11:04:15 +0900

    Modify BottomNavigationBar - 하단 내비게이션바 수정 및 완성 - 디폴트 색상이 정해지면 색상 수정 예정

--- app/src/main/java/com/example/sori/MainActivity.java
+++ app/src/main/java/com/example/sori/MainActivity.java
@@ -23,31 +23,34 @@ public class MainActivity extends AppCompatActivity {
         homeLayout = findViewById(R.id.home_ly);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        bottomNavigationViewListener();
+        BottomNavigationListener();
         bottomNavigationView.setSelectedItemId(R.id.tab_list);  //맨 처음 시작할 탭 설정
     }
 
     // 하단 네비게이션 바의 아이콘을 선택하면 해당 페이지로 이동
-    private void bottomNavigationViewListener() {
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+    private void BottomNavigationListener() {
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.tab_list:
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.tab_list: {
                         getSupportFragmentManager().beginTransaction()
                                 .replace(R.id.home_ly, new ListFragment())
                                 .commit();
-                        break;
-                    case R.id.tab_chart:
+                        return true;
+                    }
+                    case R.id.tab_chart: {
                         getSupportFragmentManager().beginTransaction()
                                 .replace(R.id.home_ly, new ChartFragment())
                                 .commit();
-                        break;
-                    case R.id.tab_setting:
+                        return true;
+                    }
+                    case R.id.tab_setting: {
                         getSupportFragmentManager().beginTransaction()
                                 .replace(R.id.home_ly, new SettingFragment())
                                 .commit();
-                        break;
+                        return true;
+                    }
                 }
                 return false;
             }
